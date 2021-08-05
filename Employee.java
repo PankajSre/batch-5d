4346e9781cb12101df2b5211ae21543afe5fe455
PankajSre <pankaj.saini85@gmail.com>
2021-08-05 11:40:31 +0530

    employee modified

--- Employee.java
+++ Employee.java
@@ -2,5 +2,12 @@ public class Employee {
    public static void main(String[] args){
      System.out.println("Hello Git repo");
 	 System.out.println("Hello Again from Git repo");
+	 
+	 System.out.println("The Sum is : "+ add(2,3));
+   }
+   
+   public static int add(int a, int b){
+	   
+	   return a + b;
    }
 }
